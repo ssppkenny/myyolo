1b6fdad8685fe370e6a3f775b53406cb4620cc98
Sergey Mikhno <sergey.mikhno@galenica.com>
2025-06-22 18:26:12 +0200

    json

--- app.py
+++ app.py
@@ -1,17 +1,55 @@
 from flask import Flask
 from doclayout_yolo import YOLOv10
+
+import os
+import json
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
 model = YOLOv10("doclayout_yolo_docstructbench_imgsz1024.pt")
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello_world():
-    filename = "dvurog_p73.png"
-    det_res = model.predict(
-        filename,   # Image to predict
-        imgsz=1024,        # Prediction image size
-        conf=0.2,          # Confidence threshold
-        device="cpu"    # Device to use (e.g., 'cuda:0' or 'cpu')
-    )
 
-    return "<p>" + str(det_res[0].boxes.xyxy) + "</p>"
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=['GET', 'POST'])
+def doc_layout():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('no file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            det_res = model.predict(
+                filename,   # Image to predict
+                imgsz=1024,        # Prediction image size
+                conf=0.2,          # Confidence threshold
+                device="cpu"    # Device to use (e.g., 'cuda:0' or 'cpu')
+            )
+            names = det_res[0].names
+            blocknames = [names[int(n)] for n in det_res[0].boxes.cls]
+            xyxy = [str(tuple(a.tolist())) for a in det_res[0].boxes.xyxy]
+            res = {key: value for key, value in zip(xyxy, blocknames)}
+
+            return json.dumps(res)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
