dddef8ab6222eb47a424fb7dbcf24cb2ef8dff90
Sergey Mikhno <sergey.mikhno@galenica.com>
2025-06-22 12:32:01 +0200

    added files

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from doclayout_yolo import YOLOv10
+model = YOLOv10("doclayout_yolo_docstructbench_imgsz1024.pt")
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    filename = "dvurog_p73.png"
+    det_res = model.predict(
+        filename,   # Image to predict
+        imgsz=1024,        # Prediction image size
+        conf=0.2,          # Confidence threshold
+        device="cpu"    # Device to use (e.g., 'cuda:0' or 'cpu')
+    )
+
+    return "<p>" + str(det_res[0].boxes.xyxy) + "</p>"
